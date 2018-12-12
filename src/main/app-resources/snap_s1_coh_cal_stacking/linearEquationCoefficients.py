e47f0fab1c548a0cf88e2751105978d2c01c71b2
Cesare Rossi <cesare.rossi@terradue.com>
2018-12-12 12:04:24 +0100

    Added debug to snap_s1_coh_cal_stacking/linearEquationCoefficients.py

--- src/main/app-resources/snap_s1_coh_cal_stacking/linearEquationCoefficients.py
+++ src/main/app-resources/snap_s1_coh_cal_stacking/linearEquationCoefficients.py
@@ -6,6 +6,11 @@ x_max=float(sys.argv[2])
 y_min=float(sys.argv[3])
 y_max=float(sys.argv[4])
 
+print "x_min=", x_min
+print "x_max=", x_max
+print "y_min=", y_min
+print "y_max=", y_max
+
 a = y_min/x_min + y_max/(x_max-x_min) - y_min*(x_max/x_min)/(x_max-x_min)
 b = (y_min*x_max-y_max*x_min)/(x_max-x_min)
 
